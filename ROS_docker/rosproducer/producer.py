408df00ae8fff3a0094af0fce3a07af3784922c1
thejaswiamarendra <thejaswia03@gmail.com>
2023-07-12 19:59:47 +0530

    ROS docker is working

---
+++ ROS_docker/rosproducer/producer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+message_publisher = rospy.Publisher("test", String, queue_size=10)
+rospy.init_node("pub", anonymous = True)
+print("Sending now")
+for i in range(100):
+    message_publisher.publish("hello")
+
+print("Sending over")
