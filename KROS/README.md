f34aa407fe58242c66e4d54eaf0e3bb2b7d35875
thejaswiamarendra <thejaswia03@gmail.com>
2023-07-12 20:01:55 +0530

    ROS docker is working

--- KROS/README.md
+++
@@ -1,8 +0,0 @@
-# KROS
-
-## KROSpublisher
-Publishes message on the given topic both locally and over the cloud
-
-
-## KROSsubscriber
-Receives message asynchronously both locally and from the cloud
--- Latency/Kafka-Kafka-Cloud/plot.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-
-df = pd.read_csv("values.csv")
-print(df["time"])
-plt.scatter(np.array(df["bytes"])[:, None], np.array(df["time"])[:, None])
-plt.show()
